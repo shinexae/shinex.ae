9740554463bd9df9e0c4e0aeab464a608ddb1d3b
Jacob Dmn <74993010+jacobdmn@users.noreply.github.com>
2023-06-09 19:31:04 +0400

    last feedback

--- src/lib/homepage.ts
+++ src/lib/homepage.ts
@@ -61,7 +61,7 @@ export const polishingPackages = [
   },
   {
     title: "STEP2",
-    menu: ["box3_p1", "box3_p2", "box3_p3", "box3_p4", "box3_p5", "box4_p6"],
+    menu: ["box3_p1", "box3_p2", "box3_p3", "box3_p4", "box3_p5"],
     prices: {
       sm: 200,
       lg: 250,
